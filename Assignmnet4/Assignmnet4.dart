18029ac80e7abdff923bfee3b9854dd184639be4
yashpflutter <flutter.yashpatil@gmail.com>
2024-02-08 15:07:14 +0530

    ass4

--- Assignmnet4/Assignmnet4.dart
+++ Assignmnet4/Assignmnet4.dart
@@ -7,7 +7,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,17 +22,18 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
   final String title;
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _couter = 0;
 
-  void _incrementCounter() {
+  void _incrementConunter() {
     setState(() {
-      _counter++;
+      _couter++;
     });
   }
 
@@ -48,21 +48,19 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'CORE2WEB',
-            ),
+            const Text('You have pushed the button this many times'),
             Text(
-              '',
+              '$_couter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: _incrementConunter,
+        tooltip: 'Incremnet',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
